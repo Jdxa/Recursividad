7bd5b12e6fa4de3f84fdde2beb606fe2e18bb10d
Jdxa <dossantosjoaquin1@gmail.com>
2025-05-23 19:11:04 -0300

    testeo

--- tpRe.java
+++ tpRe.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;
+
 public class tpRe {
     public static void main(String[] args) {
-        System.out.println("hola mundo");
+        System.out.println(imprimir(4));
+        Scanner leer = new Scanner(System.in);
+        int [] arr = {1,2,3,4};
+        impArr(arr);
+        
+    }
+    public static String imprimir(int num){
+        String st = "";
+        if (num == 0) {
+            st = "";
+        }else{
+            st = "Hola mundo ";
+            st = st + imprimir(num -1);
+        }
+        return st;
+    }
+    public static void impArr(int [] arr){
+        for (int i = 0 ; i < arr.length ; i++){
+            System.out.print(arr[i]);
+        } 
+    }
+    public static int [] cargar (Scanner leer){
+        System.out.println("Ingrese el largo del arr");
+        int p = leer.nextInt();
+        int [] arr = new int[p];
+
+        for (int i = 0 ; i < arr.length; i=i+2){
+            arr[i] = leer.nextInt();
+        }
+        return arr;
     }
 }
+
