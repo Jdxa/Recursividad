9c907b0671bd06b2a0f08b8440b5c4aaf755fad5
Jdxa <dossantosjoaquin1@gmail.com>
2025-05-24 15:18:31 -0300

    comentarios

--- bin.java
+++ bin.java
@@ -1,14 +1,21 @@
+import java.util.Scanner;
+
 public class bin {
     public static void main(String[] args) {
-        int bin1 = decToBin(8);
-        System.out.println(bin1);
+        Scanner leer = new Scanner(System.in);
+        System.out.print("Ingrese un numero y te lo paso a bin: ");
+        int num = leer.nextInt();
+        int bin1 = decToBin(num);
+        System.out.print(num+" a binarios es: " +bin1);
+        leer.close();
     }
     public static int decToBin(int dec){
         int bin, resto;
         
         if (dec > 0){
             //Saco el resto 
-            resto = dec % 2;                   
+            resto = dec % 2; 
+            //Sumo el resto con el siguiente bin                  
             bin = resto+ + 10 * decToBin(dec/2);        
             
         }else{
