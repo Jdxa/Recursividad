554e5be5a49a6125cc6bd9241c4102c31ce0695b
Jdxa <dossantosjoaquin1@gmail.com>
2025-06-03 12:02:05 -0300

    comentarios

--- Recur/tpReP18.java
+++ Recur/tpReP18.java
@@ -1,6 +1,7 @@
 package Recur;
 public class tpReP18 {
     public static void main(String[] args) {
+        //Explota el n si es mayor a 3 en dos partes donde una mitad es: n/b y la otra es n-n/b
         explotar(15, 3);
     }
     public static void explotar(int n, int b){
